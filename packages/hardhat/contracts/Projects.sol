6e69898904bbe1a73cafb1bb7d674943e64b35c6
cristianchaparroa <cristianchaparroa@gmail.com>
2023-12-03 00:53:48 -0500

    allow to add stages

--- packages/hardhat/contracts/Projects.sol
+++ packages/hardhat/contracts/Projects.sol
@@ -1,7 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+
+// The projects contract is in charge to handle all logic related to Projects
 contract Projects {
+
+    // Stage where the project is currently.
+    struct Stage {
+        // current progress percentage
+        uint256 percentage;
+    }
 
     // Project struct contains the information related to the project
     struct Project {
@@ -9,6 +17,10 @@ contract Projects {
         string title;
         // Current state of the project
         string state;
+        // The project stages
+        mapping (uint => Stage) stages;
+        // number of stages
+        uint stagesSize;
     }
 
     // projects contains the projects that currently exist on the system.
@@ -16,6 +28,20 @@ contract Projects {
 
     // create a new project add a new project to the map
     function createProject(string calldata title) public payable {
-        projects[msg.sender] = Project(title, "created");
+        projects[msg.sender].title=title;
+    }
+
+    // The add stage allow to the onwer to the specific project create a new Project stage
+    function addStage(uint256  percentage) public payable {
+        Project storage project = projects[msg.sender];
+        project.stages[project.stagesSize].percentage = percentage;
+        project.stagesSize += 1;
+    }
+
+    function getStage(uint index) public view returns(Stage memory){
+        Project storage project = projects[msg.sender];
+        require(index < project.stagesSize, "there is not a stage in the index");
+        Stage memory stage = project.stages[index];
+        return stage;
     }
 }
